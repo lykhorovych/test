2160517659a9b89b548797829ec73e6f7f123653
olykhorovych <olehlykhorovych@gmail.com>
2024-04-01 13:30:44 +0300

    v.0.5

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ import pytest
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless=new')
-    service = webdriver.ChromeService(service_args=['--log-level=INFO'], log_output=subprocess.STDOUT)
+    service = webdriver.ChromeService(service_args=['--log-level=DEBUG'], log_output=subprocess.STDOUT)
     driver = webdriver.Chrome(options=options, service=service)
     driver.set_page_load_timeout(10)
 
